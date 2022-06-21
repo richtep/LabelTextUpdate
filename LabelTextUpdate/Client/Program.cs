9136f22e24ee7d9a6909121288d4f2ac176ec761
Pavel Richter <pavel.richter.cista@gmail.com>
2022-06-21 15:47:43 +0200

    removed HttpCaller

--- LabelTextUpdate/Client/Program.cs
+++ LabelTextUpdate/Client/Program.cs
@@ -1,5 +1,4 @@
 using LabelTextUpdate.Client;
-using LabelTextUpdate.Client.HttpCaller;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,6 +8,4 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<LabelTextCaller>();
-
 await builder.Build().RunAsync();
--- LabelTextUpdate/Server/RHub/LabelTextHub.cs
+++ LabelTextUpdate/Server/RHub/LabelTextHub.cs
@@ -4,6 +4,5 @@ namespace LabelTextUpdate.Server.RHub
 {
     public class LabelTextHub : Hub
     {
-        public async Task ReadCurrentLabelText(string text) => await Clients.All.SendAsync("CommunicateLabelText", text);
     }
 }
